838c87e7e1febdf3d79503dc654fd46ed0286aea
Juan Castro <suciokastro@gmail.com>
2015-06-29 21:28:32 -0500

    create directories and copy images to thumbs folders

--- thumbnails.go
+++ thumbnails.go
@@ -2,14 +2,24 @@ package thumbnails
 
 import (
 	"errors"
-	"fmt"
 	"github.com/spf13/viper"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var configFile string
+var folder_path string
+var thumbs_path string
+var thumb_sizes map[string]string
 
 func Config(file string) (err error) {
 	if exists(file) {
@@ -29,7 +39,9 @@ func Generate(image string, overwrite bool) (err error) {
 		return err
 	}
 
-	folder_path := viper.GetString("images_folder")
+	folder_path = strings.TrimRight(viper.GetString("images_folder"), "/")
+	thumbs_path = strings.TrimRight(viper.GetString("thumbs_folder"), "/")
+	thumb_sizes = viper.GetStringMapString("sizes")
 
 	if !exists(folder_path) {
 		err = errors.New("Images folder doesn't exists")
@@ -42,27 +54,148 @@ func Generate(image string, overwrite bool) (err error) {
 	if image == "" {
 		images, _ := ioutil.ReadDir(folder_path)
 		for _, f := range images {
-			mime, err := getContentType(folder_path + "/" + f.Name())
-
-			if err != nil {
-				return err
+			if isImage(folder_path + "/" + f.Name()) {
+				err = generateThumbnail(f.Name(), overwrite)
+				if err != nil {
+					return err
+				}
 			}
-
-			fmt.Println(f.Name() + ":" + mime)
 		}
-	}
-
-	fmt.Println(viper.GetString("thumbs_folder"))
-
-	sizes := viper.GetStringMapString("sizes")
-
-	for name, size := range sizes {
-		fmt.Println(name + ":" + size)
 	}
 
 	return err
 }
 
+// generate one thumbnail
+func generateThumbnail(image_file string, overwrite bool) error {
+	var err error
+	if isDirectory(thumbs_path) {
+		for thumb_folder, thumb_size := range thumb_sizes {
+			thumb_folder_path := thumbs_path + "/" + thumb_folder
+			if !exists(thumb_folder_path) {
+				log.Println("Creating folder" + thumb_folder_path)
+				err := os.Mkdir(thumb_folder_path, 0755)
+				if err != nil {
+					return err
+				}
+			}
+
+			if isDirectory(thumb_folder_path) {
+				thumb_file_path := thumb_folder_path + "/" + image_file
+
+				width, height, exact_size, err := parseSize(thumb_size)
+				if err != nil {
+					return err
+				}
+
+				if exists(thumb_file_path) && overwrite == false {
+					log.Printf("Nothing to do, thumb %s already exists\n", thumb_file_path)
+				} else {
+					err = copyImage(folder_path+"/"+image_file, thumb_file_path)
+					if err != nil {
+						return err
+					}
+
+					log.Printf("resize %s %dx%d exact: %t\n", thumb_file_path, width, height, exact_size)
+				}
+			} else {
+				return errors.New("Can't create thumbnails. " + thumb_folder_path + " must be a directory")
+			}
+		}
+	} else {
+		return errors.New("Thumbs folder doesn't exist or is not a Folder")
+	}
+
+	return err
+}
+
+func getImageDimensions(imagePath string) (width int, height int, err error) {
+	file, file_err := os.Open(imagePath)
+	if file_err != nil {
+		err = file_err
+		return
+	}
+
+	image, _, image_err := image.DecodeConfig(file)
+	if image_err != nil {
+		err = image_err
+		return
+	}
+
+	width = image.Width
+	height = image.Height
+
+	return
+}
+
+func copyImage(image string, thumb_file string) error {
+	log.Println("copy " + image + " to " + thumb_file)
+	reader, err := os.Open(image)
+	if err != nil {
+		return err
+	}
+
+	defer reader.Close()
+
+	writer, err := os.Create(thumb_file)
+	if err != nil {
+		return err
+	}
+
+	defer writer.Close()
+
+	_, err = io.Copy(writer, reader)
+
+	return err
+}
+
+func parseSize(size_string string) (crop_width int, crop_height int, exact bool, err error) {
+	if !(strings.HasPrefix(size_string, "=") || strings.HasPrefix(size_string, "<")) {
+		err = errors.New("Invalid thumbnail size It should start with a < or = :" + size_string)
+		return
+	} else if !strings.Contains(size_string, "x") {
+		err = errors.New("Invalid thumbnail size: " + size_string)
+		return
+	} else {
+		width_height := strings.Split(strings.TrimLeft(size_string, "<="), "x")
+
+		crop_width, err = strconv.Atoi(width_height[0])
+		if err != nil {
+			err = errors.New("width must be an integer")
+			return
+		}
+
+		crop_height, err = strconv.Atoi(width_height[1])
+		if err != nil {
+			err = errors.New("height must be an integer")
+			return
+		}
+
+		if strings.HasPrefix(size_string, "=") {
+			exact = true
+		} else {
+			exact = false
+		}
+		return
+	}
+}
+
+// check if file is an image
+func isImage(image_path string) bool {
+	mime, err := getContentType(image_path)
+
+	if err != nil {
+		return false
+	} else {
+		if mime == "image/gif" || mime == "image/jpeg" || mime == "image/png" {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+// get content type of file
 func getContentType(path string) (mime string, err error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -89,6 +222,7 @@ func isDirectory(path string) bool {
 	}
 }
 
+// check if file exists
 func exists(file string) bool {
 	if _, err := os.Stat(file); err == nil {
 		return true
